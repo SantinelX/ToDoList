c8df03618cb3384c6d51b6b3cd44dd4f770fd352
Calin Balazs <balazs_calin@yahoo.com>
2021-04-17 13:30:20 +0300

    loginImplementation

---
+++ src/main/java/com/todolist/todolist/controller/RoleController.java
@@ -0,0 +1,29 @@
+package com.todolist.todolist.controller.dtos;
+
+import com.todolist.todolist.model.Role;
+import com.todolist.todolist.services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class RoleController {
+    private RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService){
+        this.roleService = roleService;
+    }
+
+    @PostMapping(path = "/roles")
+    public Role create(@RequestBody Role role){
+        return roleService.create(role);
+    }
+
+    @GetMapping(path = "/roles/{roleName}")
+    public Role findRoleName(@PathVariable String rolename) {
+        return roleService.findByRoleName(rolename);
+
+    }
+}
